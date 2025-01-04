24085cf780976dcd0cea946a57531e1f9647f2bb
jozfre <johanzafrijohar@gmail.com>
2025-01-05 02:43:40 +0800

    login page ui and app.css

--- ess-mbtho/src/routes/+page.svelte
+++ ess-mbtho/src/routes/+page.svelte
@@ -1,3 +1,43 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+  <script lang="ts">
+    let { data } = $props();
+
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+  </script>
+
+<div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+  <div class="flex items-center justify-center py-12">
+    <div class="mx-auto grid w-[350px] gap-6">
+      <div class="grid gap-2 text-center">
+        <h1 class="text-3xl font-bold">Login</h1>
+        <p class="text-muted-foreground text-balance">
+          Enter your email below to login to your account
+        </p>
+      </div>
+      <div class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <div class="flex items-center">
+            <Label for="password">Password</Label>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" class="w-full">Login</Button>
+        <Button variant="outline" class="w-full">Continue as Organizer</Button>
+      </div>
+    </div>
+  </div>
+  <div class="bg-muted hidden lg:block">
+    <img
+      src="/images/favicon.png"
+      alt="logo mbtho"
+      width="1920"
+      height="1080"
+      class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+    />
+  </div>
+</div>
