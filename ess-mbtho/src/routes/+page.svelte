b15ca20ee9906aeae71398566cc50f702fd98775
jozfre <johanzafrijohar@gmail.com>
2025-03-08 16:57:26 +0800

    remove unwanted files

--- ess-mbtho/src/routes/+page.svelte
+++ ess-mbtho/src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import { Button } from "$lib/components/ui/button/index.js";
     import { Input } from "$lib/components/ui/input/index.js";
     import { Label } from "$lib/components/ui/label/index.js";
+
   </script>
 
 <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
